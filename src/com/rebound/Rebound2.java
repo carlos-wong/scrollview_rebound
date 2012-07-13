b0a89aeab8daa347a4ae46b2723ee9aaae10442c
carlos <huaixian.huang@gmail.com>
2012-07-13 08:51:19 +0800

    base file work

---
+++ src/com/rebound/Rebound2.java
@@ -0,0 +1,71 @@
+package com.rebound;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class Rebound2 extends Activity
+{
+
+    private ScrollView scroll;
+
+    private TextView tv;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main1);
+        scroll = (ScrollView) findViewById(R.id.sv);
+        tv = (TextView) findViewById(R.id.tv);
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+    }
+
+    @Override
+    public void onAttachedToWindow()
+    {
+
+        super.onAttachedToWindow();
+        scrollToBottom(scroll, tv);
+    }
+
+    public void scrollToBottom(final View scroll, final View inner)
+    {
+
+        Handler mHandler = new Handler();
+
+        mHandler.post(new Runnable()
+        {
+            public void run()
+            {
+                if (scroll == null || inner == null)
+                {
+                    return;
+                }
+
+                int offset = inner.getMeasuredHeight() - scroll.getHeight();
+                if (offset < 0)
+                {
+                    offset = 0;
+                }
+
+                scroll.scrollBy(0, offset+100);
+               // scroll.scrollBy(0, -offset);
+                System.out.println("scrollX:" + scroll.getScrollX() + "scrollY:" + scroll.getScrollY());
+            }
+        });
+    }
+
+}
